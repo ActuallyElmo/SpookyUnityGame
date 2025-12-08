570e769994accdb2a2d24a10dfbad9897cdeea5c
MIHAI DANAILA <danailateodor@yahoo.com>
2025-12-08 22:31:15 +0200

    Implemented Opening Doors

---
+++ Assets/Scripts/Environment/Doors/DoorRaycast.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DoorRaycast : MonoBehaviour
+{
+    [SerializeField] private int rayDistance = 5;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private string excludeLayerName = null;
+
+    [Header("Input Action Asset")]
+    [SerializeField] private InputActionAsset playerControls;
+
+    [Header("Action Map Name Reference")]
+    [SerializeField] private string actionMapName = "Player";     // Action map used for player controls
+
+    [Header("Action Name References")]
+    [SerializeField] private string interact = "Interact";
+    private InputAction interactAction;
+
+    private const string doorTag = "Door";
+    private DoorController raycastedDoor = null;
+
+    private void Awake()
+    {
+        InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
+
+        interactAction = mapReference.FindAction(interact);
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+
+        int mask = 1 << LayerMask.NameToLayer(excludeLayerName) | layerMask.value;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, mask))
+        {
+            if (hit.collider.CompareTag(doorTag))
+            {
+                if (!raycastedDoor)
+                {
+                    raycastedDoor = hit.collider.gameObject.GetComponent<DoorController>();
+                    Console.WriteLine("Found Door");
+                }
+
+                if (interactAction.WasPressedThisFrame())
+                {
+                    raycastedDoor.PlayAnimation();
+                }
+            }
+        }
+        else
+        {
+            raycastedDoor = null;
+        }
+    }
+}
