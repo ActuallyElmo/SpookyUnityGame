25a2be0bb571a0fe610d6e59950284c88a685693
Andrei <andrei@seniuc.ro>
2026-01-14 12:04:09 +0200

    Fixed environment interactions

---
+++ Assets/Scripts/Player/EnvInteraction.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class EnvInteraction : MonoBehaviour
+{
+    [SerializeField] float pickUpRange = 3f;
+    private const string objectLayer = "InteractObjects";
+
+    private IInteractable foundObject = null;
+
+    private PlayerInputHandler inputHandler;
+    void Awake()
+    {
+        inputHandler = GetComponent<PlayerInputHandler>(); // Get input handler from the player
+    }
+
+    private void CheckRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+
+        int layerMask = LayerMask.GetMask(objectLayer);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, pickUpRange, layerMask))
+        {
+            IInteractable interactableObject = hit.collider.GetComponent<IInteractable>();
+
+            if(interactableObject != null)
+            {
+                foundObject = interactableObject;
+                HudController.Instance.EnableInteractionText("(E)", "Interact");
+            }
+            
+        }
+        else {
+            
+            if(foundObject != null)
+            {
+                foundObject = null;
+                HudController.Instance.DisableInteractionText();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckRay();
+
+        if (inputHandler != null && inputHandler.InteractTriggered && foundObject != null)
+        {
+            foundObject.Interact();
+        }
+    }
+}
